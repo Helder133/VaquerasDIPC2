c59509978d8d7c65b50dfe62ccc9d660dc3f7e62
Helder133 <hrljuar@gmail.com>
2025-12-11 16:47:43 -0600

    comenzando por las configuraciones y dependencias a usar en el sistema

---
+++ segunda y tercera entrega/vaquerasDIPC2/src/main/java/ipc2_vaquerad/vaquerasdipc2/JakartaRestConfiguration.java
@@ -0,0 +1,17 @@
+package ipc2_vaquerad.vaquerasdipc2;
+
+import jakarta.ws.rs.ApplicationPath;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("api/v1")
+public class JakartaRestConfiguration extends ResourceConfig {
+    public JakartaRestConfiguration() {
+        packages("ipc2_vaquerad.vaquerasdipc2.resources").register(MultiPartFeature.class);
+
+    }
+}
